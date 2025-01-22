2e30b97e57de73f2493c1eeb6b624beaf4fe8cf0
Neon Ventura <neonventura@gmail.com>
2025-01-22 13:40:10 -0300

    post created

--- src/main/java/com/apib/demo/controllers/ProductsController.java
+++ src/main/java/com/apib/demo/controllers/ProductsController.java
@@ -4,10 +4,7 @@ import com.apib.demo.models.Product;
 import com.apib.demo.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,10 +16,13 @@ public class ProductsController {
     private ProductRepository productRepository;
 
     @GetMapping
-    public ResponseEntity<List<Product>> getAllProducts() {
-        // Busca todos os produtos no banco de dados
-        List<Product> products = productRepository.findAll();
-        return ResponseEntity.ok(products);
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping
+    public void postProduct(@RequestBody Product product) {
+        productRepository.save(product);
     }
 
 }
